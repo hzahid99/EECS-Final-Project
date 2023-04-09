8598a5d1e7d4cb024e302b242416be4bd5c20ed7
RokunuzJahan Rudro <61032181+rudro12356@users.noreply.github.com>
2023-04-09 17:08:40 -0500

    adding comments to app.py

--- Integration/Integrated App/Flask server/app.py
+++ Integration/Integrated App/Flask server/app.py
@@ -1,3 +1,31 @@
+"""
+This is a Flask server implementation for a parking lot car counting system. The server uses YOLOv5, a deep learning object detection model, to detect cars in images and count them. The server provides an API endpoint '/predict' that accepts a list of images and returns the total count of cars per level in the parking lot.
+
+The server uses the following modules:
+
+    Flask: A Python web framework for building APIs.
+    glob: A module that provides a function to search for files matching a specific pattern.
+    matplotlib: A module for plotting graphs and charts.
+    torch: A PyTorch library for deep learning.
+    numpy: A module for working with arrays and numerical operations.
+    pandas: A module for working with data in tabular form.
+    os: A module for interacting with the operating system.
+
+The YOLOv5 model is loaded from a specified path and used to detect the cars in the images. The results are stored in a dictionary, where each image is associated with the count and direction of the detected cars. Finally, the server returns the total count of cars per level in the parking lot.
+
+To run the server, execute this script using 'python script_name.py'. The server runs on localhost and listens on port 5000. The '/predict' endpoint accepts POST requests with a list of images as the request body. The images should be in JPEG format and stored on the server in the 'images' directory.
+
+This implementation can be improved further by adding error handling, improving the readability of the code, and using configuration files to store environment-specific variables.
+"""
+/*
+Author: Bikash, Rudro
+Date: 4/1/2023
+Revision date: 4/2/2023
+File name: app.py
+
+*/
+
+#importing libraries
 from flask import Flask, jsonify
 import glob as glob
 import matplotlib.pyplot as plt
@@ -20,14 +48,17 @@ def index():
 
 @app.route('/predict')
 def predict():
-    total_cars_per_level = 50
-
+    
+    total_cars_per_level = 0
+    
+    # loading the custom YoloV5 model using TorchHub
     model_yolo = torch.hub.load(".", 'custom', path="/Users/home/Downloads/parking-ku/Flask server/Weights/best_100.pt", source='local', force_reload=True)
-    # img = '/Users/home/Downloads/parking-ku/Flask server/images/back.jpg'
+    
     detected_cars = {}
 
     images_dir = "/Users/home/Downloads/parking-ku/Flask server/images"
-
+    
+    # loop through the images for the detection of front and back
     for filename in os.listdir(images_dir):
         if filename.endswith(".jpg"):
             img_path = os.path.join(images_dir, filename)
